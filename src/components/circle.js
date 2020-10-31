fcc565a488504490d04ffe39e056b4a61f4f585f
anishkumar <anishkumar8564037181@gmail.com>
2020-10-31 19:51:58 +0530

    inheritance code

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,14 @@
 
 
 // export class using module.exports
+import shape from './shape';
+export default class Circle extends shape {
+    constructor(color) {
+        //first line of subclass constructer should call superclass constructer with appropriate parameters;
+         super(color);
+    }
+    calculateArea() {
+        console.log("Area of circle with color: " + this.color);
+        return 3.14*5*5;
+    }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,34 @@
 // declare class
 
 // export class using module.exports
+
+export default class shape {
+    constructor(color) {
+        this.color = color;
+
+    }
+    drawshape() {
+        console.log("Drawing a Shape");
+    }
+    calculateArea() {
+        console.log("Do't know area of Unknown Shape");
+        return 0;
+    }
+}
+// class Circle extends shape {
+//     constructor(color) {
+//         //first line of subclass constructer should call superclass constructer with appropriate parameters;
+//          super(color);
+//     }
+//     calculateArea() {
+//         console.log("Area of circle with color: " + this.color);
+//         return 3.14*5*5;
+//     }
+// }
+
+// class GreenCircle extends Circle {
+//      //first line of subclass constructer should call superclass constructer with appropriate parameters;
+//     constructor() {
+//         super("green");
+//     }
+// }
