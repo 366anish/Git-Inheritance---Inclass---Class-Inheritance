eac75ccaa6bce594a4930c947ec19c46c1ad2d35
anishkumar <anishkumar8564037181@gmail.com>
2020-11-01 13:11:16 +0530

    inheritence code

--- src/components/circle.js
+++ src/components/circle.js
@@ -5,9 +5,9 @@
 
 // export class using module.exports
 // import shape from './shape' ;
-const shape = require('./shape');
+const Shape = require('./shape');
 class Circle extends Shape {
-    
+
     constructor() {
         //first line of subclass constructer should call superclass constructer with appropriate parameters;
          super();
